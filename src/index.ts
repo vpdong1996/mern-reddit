91442754e5a8f30e494fbcaa057b74475a5c1cbc
DongVu <dongkg3876@gmail.com>
2021-04-12 20:03:22 +0700

    Finish Mikro and setup login register func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello Chiec")
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import config from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+const main = async () => {
+  const orm = await MikroORM.init(config);
+  await orm.getMigrator().up();
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+  app.listen(4100, () => console.log("Server started on localhost:4100"));
+};
+
+main().catch((err) => console.log("Error", err));
